e7916aea1883f4de07fa0819cc66365a375c5fd2
jaewoong2 <jwisgenius@naver.com>
2024-02-21 00:01:26 +0900

    :goal_net: Error Code 추가

--- src/common/exception/error-code/error.code.ts
+++ src/common/exception/error-code/error.code.ts
@@ -14,3 +14,7 @@ export type ErrorCode = ErrorCodeVo;
 // 아래에 에러코드 값 객체를 생성
 // Create an error code instance below.
 export const ENTITY_NOT_FOUND = new ErrorCodeVo(404, 'Entity Not Found');
+export const CLIENT_ERROR = new ErrorCodeVo(
+  400,
+  'Client Error: 잘못된 값 입니다',
+);
