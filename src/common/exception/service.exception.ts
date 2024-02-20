9cff603e672cec637c2bb42be05ae22d7eacb5b3
jaewoong2 <jwisgenius@naver.com>
2024-02-21 00:00:46 +0900

    :goal_net: Error Exception 생성 메서드 추가

--- src/common/exception/service.exception.ts
+++ src/common/exception/service.exception.ts
@@ -1,10 +1,14 @@
 // ENTITY_NOT_FOUND 값 객체(status, default-message)를 가진
 
-import { ENTITY_NOT_FOUND, ErrorCode } from './error-code';
+import { CLIENT_ERROR, ENTITY_NOT_FOUND, ErrorCode } from './error-code';
 
 //  ServiceException 인스턴스 생성 메서드
 export const EntityNotFoundException = (message?: string): ServiceException => {
   return new ServiceException(ENTITY_NOT_FOUND, message);
+};
+
+export const ClientErrorException = (message?: string): ServiceException => {
+  return new ServiceException(CLIENT_ERROR, message);
 };
 
 export class ServiceException extends Error {
